ce7f0d55ece2ea1754d25c5d077772ea96278706
nwleedev <nw.lee@outlook.com>
2024-04-07 01:39:10 +0900

    feat: idioms grid component

---
+++ app/components/GridIdiom/index.tsx
@@ -0,0 +1,67 @@
+import { CSSProperties } from "react";
+import { Link } from "react-router-dom";
+import { Idiom } from "~/types/idiom";
+import { GuardV3 } from "../Guard";
+
+interface GridIdiomProps {
+  idiom?: Idiom;
+  columnIndex: number;
+  columns: number;
+  style: CSSProperties;
+}
+
+const GridIdiom = (props: GridIdiomProps) => {
+  const { idiom, columnIndex, columns, style } = props;
+  const classNames = [
+    columnIndex === 0
+      ? "pr-2 pl-1"
+      : columnIndex === columns - 1
+      ? "pl-2 pr-2"
+      : "px-2",
+    "py-2",
+    "w-full max-w-full h-full mx-auto",
+  ];
+
+  return (
+    <GuardV3 data={{ idiom }} when={!!idiom}>
+      {({ data: { idiom } }) => (
+        <div className={classNames.join(" ")} style={{ ...style }}>
+          <Link
+            key={"idiom:" + idiom.id}
+            to={`/${idiom.id}`}
+            className="w-full h-full max-w-full mx-auto"
+          >
+            <section className="flex flex-col w-full h-full px-4 pt-6 pb-4 rounded shadow cursor-pointer gap-y-2 bg-slate-50 hover:shadow-lg">
+              <div
+                style={{
+                  backgroundImage: `url("https://static.useidioms.com/${idiom.thumbnail}")`,
+                }}
+                className="bg-center"
+              >
+                <div className="backdrop-blur-[50px]">
+                  <img
+                    src={`https://static.useidioms.com/${idiom.thumbnail}`}
+                    alt={idiom.idiom}
+                    className="max-w-[190px] w-full object-cover mx-auto h-auto"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-col h-full">
+                <h2 className="mt-1 text-lg font-semibold leading-tight font-Work_Sans line-clamp-2">
+                  {idiom.idiom}
+                </h2>
+                <div className="mt-auto mb-2">
+                  <p className="text-xs font-normal line-clamp-2">
+                    {idiom.meaningBrief}
+                  </p>
+                </div>
+              </div>
+            </section>
+          </Link>
+        </div>
+      )}
+    </GuardV3>
+  );
+};
+
+export default GridIdiom;
---
+++ app/components/IdiomsGrid/index.tsx
@@ -0,0 +1,154 @@
+import { useEffect, useRef, useState } from "react";
+import { FixedSizeGrid } from "react-window";
+import InfiniteLoader from "react-window-infinite-loader";
+import { Idiom } from "~/types/idiom";
+import GridIdiom from "../GridIdiom";
+import { GuardV3 } from "../Guard";
+
+import useGridColumns from "~/hooks/useGridColumns";
+import style from "./index.scss?url";
+
+export const css = [{ rel: "stylesheet", href: style }];
+
+export interface IdiomsGridProps {
+  idioms?: Idiom[];
+  isFetched?: boolean;
+  isFetching?: boolean;
+  hasNextPage?: boolean;
+  fetchNextPage?: () => unknown;
+}
+
+const fallback = () => {
+  console.warn("The function is not implemented");
+};
+
+const IdiomsGrid = (props: IdiomsGridProps) => {
+  const {
+    idioms,
+    isFetched,
+    isFetching,
+    hasNextPage,
+    fetchNextPage = fallback,
+  } = props;
+  const gridRef = useRef(null as HTMLElement | null);
+  const [gridHeight, setGridHeight] = useState(480);
+  const [gridWidth, setGridWidth] = useState(0);
+  const { columns, maxWidth } = useGridColumns();
+
+  useEffect(() => {
+    const observer = new ResizeObserver((entries) => {
+      entries.forEach((entry) => {
+        if (!entry.target.isSameNode(document.body)) {
+          return;
+        }
+        if (gridRef.current?.clientHeight !== undefined) {
+          const height = gridRef.current?.clientHeight - 1;
+          setGridHeight(height);
+        }
+      });
+    });
+    observer.observe(document.body);
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+  useEffect(() => {
+    const gridCopy = gridRef.current;
+    if (!isFetched || !gridCopy) {
+      return;
+    }
+    const initialWidth = gridCopy.clientWidth;
+    if (initialWidth) {
+      setGridWidth(initialWidth);
+    }
+    if (gridRef.current?.clientHeight !== undefined) {
+      const height = gridRef.current?.clientHeight;
+      setGridHeight(height - 1);
+    }
+  }, [isFetched]);
+  useEffect(() => {
+    const observer = new ResizeObserver((entries) => {
+      entries.forEach((entry) => {
+        if (!entry.target.isSameNode(document.body)) {
+          return;
+        }
+        if (!gridRef.current) {
+          return;
+        }
+        setGridWidth(gridRef.current.clientWidth);
+      });
+    });
+    observer.observe(document.body);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+  return (
+    <GuardV3 data={{ idioms }} when={!!idioms}>
+      {({ data: { idioms } }) => {
+        return (
+          <article
+            className={["flex w-full h-full", maxWidth].join(" ")}
+            ref={(element) => (gridRef.current = element)}
+          >
+            <InfiniteLoader
+              itemCount={idioms.length + 1}
+              isItemLoaded={(index) => idioms[index] !== undefined}
+              loadMoreItems={() => {
+                if (hasNextPage && !isFetching) {
+                  fetchNextPage();
+                }
+              }}
+            >
+              {({ onItemsRendered, ref }) => {
+                return (
+                  <FixedSizeGrid
+                    onItemsRendered={(renderProps) => {
+                      const {
+                        visibleRowStartIndex,
+                        visibleRowStopIndex,
+                        overscanRowStartIndex,
+                        overscanRowStopIndex,
+                      } = renderProps;
+                      return onItemsRendered({
+                        visibleStartIndex: visibleRowStartIndex * columns,
+                        visibleStopIndex: visibleRowStopIndex * columns,
+                        overscanStartIndex: overscanRowStartIndex * columns,
+                        overscanStopIndex: overscanRowStopIndex * columns,
+                      });
+                    }}
+                    className="!overflow-x-hidden !overflow-y-scroll idiomsGrid"
+                    ref={ref}
+                    width={gridWidth}
+                    height={gridHeight}
+                    rowCount={
+                      columns !== 0 ? Math.ceil(idioms.length / columns) : 0
+                    }
+                    rowHeight={360}
+                    columnCount={columns}
+                    columnWidth={columns !== 0 ? (gridWidth - 8) / columns : 0}
+                  >
+                    {({ rowIndex, columnIndex, style }) => {
+                      const idiomIndex = rowIndex * columns + columnIndex;
+                      return (
+                        <GridIdiom
+                          idiom={idioms[idiomIndex]}
+                          columnIndex={columnIndex}
+                          columns={columns}
+                          style={style}
+                        />
+                      );
+                    }}
+                  </FixedSizeGrid>
+                );
+              }}
+            </InfiniteLoader>
+          </article>
+        );
+      }}
+    </GuardV3>
+  );
+};
+
+export default IdiomsGrid;
