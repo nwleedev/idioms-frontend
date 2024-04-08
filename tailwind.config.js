33b05c7e2a66cff6a8a712704d8bc340393fe814
nwleedev <nw.lee@outlook.com>
2024-04-08 22:54:56 +0900

    fix: use default theme also

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
+
 export default {
+  ...defaultTheme,
   content: ["./app/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontSize: {
+        mini: "0.625rem",
+      },
       screens: {
         column200: "495px",
         column300: "735px",
