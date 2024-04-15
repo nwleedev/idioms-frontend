a6cd05d48b7cea235a93da471e5dee039553f1f6
nwleedev <nw.lee@outlook.com>
2024-04-15 13:38:12 +0900

    fix: button actions

--- app/components/Button/index.tsx
+++ app/components/Button/index.tsx
@@ -4,10 +4,17 @@ export interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
   className?: string;
   children?: ReactNode;
   fontSize?: "xs" | "sm" | "base";
+  action?: "primary" | "secondary" | "info" | "success" | "danger";
 }
 
 const Button = (props: ButtonProps) => {
-  const { children, className = "", fontSize, ...others } = props;
+  const {
+    children,
+    className = "",
+    fontSize,
+    action = "primary",
+    ...others
+  } = props;
   const fontSizeStyle = useMemo(() => {
     switch (fontSize) {
       case "xs": {
@@ -23,13 +30,30 @@ const Button = (props: ButtonProps) => {
         return "text-sm";
     }
   }, [fontSize]);
+  const background = useMemo(() => {
+    switch (action) {
+      case "primary":
+        return "bg-red-500 text-white";
+      case "secondary":
+        return "bg-white border-red-500 text-red-500 border";
+      case "success":
+        return "bg-green-600 text-white";
+      case "danger":
+        return "bg-black text-white";
+      case "info":
+        return "bg-sky-500 text-white";
+    }
+  }, [action]);
+  const defaultClassname =
+    "inline-flex rounded-lg gap-x-0.5 font-semibold py-1 px-3";
   const classNames = [
+    defaultClassname,
     fontSizeStyle,
-    "inline-flex rounded-lg gap-x-0.5 font-semibold bg-red-500 text-white py-1 px-3",
+    background,
     className,
-  ];
+  ].join(" ");
   return (
-    <button className={classNames.join(" ")} {...others}>
+    <button className={classNames} {...others}>
       {children}
     </button>
   );
