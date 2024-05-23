a35b00f744e7272f7f4bf658fb225d506cb7fc48
nwleedev <nw.lee@outlook.com>
2024-05-23 23:27:17 +0900

    feat: Footer

---
+++ app/components/Footer/index.tsx
@@ -0,0 +1,38 @@
+import { Link } from "@remix-run/react";
+import useGridColumns from "~/hooks/useGridColumns";
+import { cs } from "~/lib/classnames";
+
+export interface FooterProps {
+  className?: string;
+}
+
+export default function Footer(props: FooterProps) {
+  const { maxWidth } = useGridColumns();
+  const { className = "" } = props;
+
+  return (
+    <footer
+      className={cs("w-full flex text-white bg-primary py-0.5 px-4", className)}
+    >
+      <div className="flex flex-col items-center justify-between w-full column200:flex-row">
+        <div className="flex text-base font-semibold">
+          <p>useidioms.com</p>
+        </div>
+        <div
+          className={cs(
+            "flex items-center gap-x-2.5 text-xs h-full py-0.5",
+            maxWidth
+          )}
+        >
+          <Link to={"/privacy-policy"}>
+            <p>Privacy Policy</p>
+          </Link>
+          <p className="w-px h-full text-white bg-white" />
+          <Link to={"/terms-and-conditions"}>
+            <p>Terms and Conditions</p>
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
+}
