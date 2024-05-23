346ba7d06584a4f496695097736862883f4484d0
nwleedev <nw.lee@outlook.com>
2024-05-23 23:23:17 +0900

    fix: mdx configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
+import mdx from "@mdx-js/rollup";
 import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
+import remarkFrontmatter from "remark-frontmatter";
+import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -12,6 +15,9 @@ export default defineConfig(() => {
   return {
     plugins: [
       tsconfigPaths(),
+      mdx({
+        remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
+      }),
       remix({
         basename: "/",
         buildDirectory: "build",
